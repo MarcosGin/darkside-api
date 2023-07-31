89f61a185c6a236f5081dd814c4d7816bee14981
Marcos Raúl Gin <marcosgin291@gmail.com>
2023-07-31 02:57:11 -0300

    refactor(config): update cache for swapi to 1 day

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export const config = {
   port: process.env.PORT || 8080,
   cache: true,
   cacheTime: 60 * 5,
-  swapiCacheTime: 60 * 10,
+  swapiCacheTime: 60 * 60 * 24, // 1day
   redisUrl: process.env.REDIS_URL,
 };
